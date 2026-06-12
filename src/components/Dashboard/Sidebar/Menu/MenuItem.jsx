58ddc3023c9564da222859398d3c5ebd190c83e4
Muyedur rahman <muyedrahman732@gmail.com>
2026-06-12 20:22:20 +0600

    implement dark mode across entire UI

--- src/components/Dashboard/Sidebar/Menu/MenuItem.jsx
+++ src/components/Dashboard/Sidebar/Menu/MenuItem.jsx
@@ -21,4 +21,28 @@
 
 // export default MenuItem
 
+import { NavLink } from "react-router";
 
+const MenuItem = ({ label, address, icon: Icon }) => {
+  return (
+    <NavLink
+      to={address}
+      end
+      className={({ isActive }) =>
+        `flex items-center px-4 py-2 my-2 rounded-md transition-colors duration-300
+        hover:bg-gray-300 dark:hover:bg-gray-800
+        hover:text-gray-700 dark:hover:text-white
+        ${
+          isActive
+            ? "bg-gray-300 dark:bg-gray-800 text-gray-700 dark:text-white"
+            : "text-gray-600 dark:text-gray-300"
+        }`
+      }
+    >
+      <Icon className="w-5 h-5" />
+      <span className="mx-4 font-medium">{label}</span>
+    </NavLink>
+  );
+};
+
+export default MenuItem;
