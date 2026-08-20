d4583ae105384774e50a7d28309c60db9c770aeb
Muyedur rahman <muyedrahman732@gmail.com>
2026-08-20 19:42:23 +0600

    d

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,14 +1,32 @@
-import { Outlet } from "react-router";
-import Sidebar from "../components/Dashboard/Sidebar/Sidebar";
+// import { Outlet } from "react-router";
+// import Sidebar from "../components/Dashboard/Sidebar/Sidebar";
+
+// const DashboardLayout = () => {
+//   return (
+//     <div className="relative min-h-screen md:flex bg-white dark:bg-gray-950 transition-colors duration-300">
+//       {/* Sidebar */}
+//       <Sidebar />
+
+//       {/* Dashboard Content */}
+//       <div className="flex-1 md:ml-64">
+//         <div className="p-5 text-gray-900 dark:text-white">
+//           <Outlet />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DashboardLayout;
 
 const DashboardLayout = () => {
   return (
-    <div className="relative min-h-screen md:flex bg-white dark:bg-gray-950 transition-colors duration-300">
+    <div className="relative min-h-screen md:flex bg-white dark:bg-gray-950 transition-colors duration-300 overflow-x-hidden">
       {/* Sidebar */}
       <Sidebar />
 
       {/* Dashboard Content */}
-      <div className="flex-1 md:ml-64">
+      <div className="flex-1 md:ml-64 w-full overflow-x-hidden">
         <div className="p-5 text-gray-900 dark:text-white">
           <Outlet />
         </div>
